f7428aaa5071d5c6a1b24e119ec95bb34280e487
Andrew Powers <andrew@pagelines.com>
2013-08-16 15:58:59 -0700

    New registration stuff

---
+++ editor/sections.register.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * PageLines Sections Registration Class
+ *
+ * An object that is used to scan and load sections, then add them to the factory object.
+ *
+ * @author 		PageLines
+ * @category 	Core
+ * @package 	DMS
+ * @version     1.0.0
+ */
+
+class PLSectionsRegister {
+
+
+	function __construct( ){
+		
+	}
+	
+	function something(){
+		
+	}
+
+
+}
