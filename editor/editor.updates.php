c66467910801b9739222b96b5535a5f05c9a73e8
Simon Prosser <simon@pagelines.com>
2013-09-06 23:07:23 +0100

    updater updates

--- editor/editor.updates.php
+++ editor/editor.updates.php
@@ -14,9 +14,10 @@ class PageLinesEditorUpdates {
 		if ( ! current_user_can( 'edit_theme_options' ) )
 			return;
 
-		add_filter( 'site_transient_update_plugins', array( $this, 'injectUpdatePlugins' ), 999 );		
+		add_filter( 'pre_set_site_transient_update_plugins', array( $this, 'injectUpdatePlugins' ), 999 );		
 		add_filter( 'site_transient_update_themes', array( $this, 'injectUpdateThemes' ), 999 );
 		add_action( 'load-update-core.php', array( $this, 'del_store_data' ) );
+		add_filter( 'plugins_api', array( $this, 'check_info' ), 10, 3 );
 	}
 	
 	function del_store_data() {
@@ -29,9 +30,6 @@ class PageLinesEditorUpdates {
 	
 	function injectUpdateThemes( $updates ) {
 
-		if( ! $this->pl_is_pro() )
-			return $updates;
-
 		global $storeapi;
 		if( ! is_object( $storeapi ) )
 			$storeapi = new EditorStoreFront;
@@ -43,9 +41,13 @@ class PageLinesEditorUpdates {
 			if( ! isset( $mixed_array[$slug]['version'] ) )
 				continue;
 				
-			if( $mixed_array[$slug]['version'] > $data['Version'] ) {
+			if( $mixed_array[$slug]['version'] > $data['Version'] && $this->pl_is_pro() ) {
 				$updates->response[$slug] = $this->build_theme_array( $mixed_array[$slug], $data );		
-			}	
+			} else {
+				if( is_object( $updates ) && isset( $updates->response ) && isset( $updates->response[$slug] ) ) {
+					unset( $updates->response[$slug] );
+				}
+			}
 		}
 		return $updates;
 	}
@@ -72,12 +74,11 @@ class PageLinesEditorUpdates {
 			if( ! isset( $mixed_array[$slug] ) ) {
 				if( is_object( $updates ) && isset( $updates->response ) && isset( $updates->response[$path] ) ) {
 					unset( $updates->response[$path] );
-					continue;
 				}
 			}
 
 			// If PageLines plugin has API data and a version check it and build a response.
-			if( isset( $mixed_array[$slug]['version'] ) && ( $mixed_array[$slug]['version'] > $data['Version'] ) ) {
+			if( isset( $mixed_array[$slug]['version'] ) && ( $mixed_array[$slug]['version'] > $data['Version'] ) && $this->pl_is_pro() ) {
 				$updates->response[$path] = $this->build_plugin_object( $mixed_array[$slug], $data );
 			} else {
 				if( is_object( $updates ) && isset( $updates->response ) && isset( $updates->response[$path] ) ) {
@@ -102,14 +103,51 @@ class PageLinesEditorUpdates {
 	function build_plugin_object( $api_data, $data ) {
 
 		$object = new stdClass;		
-		$object->id = rand();
-		$object->slug = rand();
+//		$object->id = rand();
+		$object->slug = $api_data['slug'];
 		$object->new_version = $api_data['version'];
 		$object->upgrade_notice = 'This is a PageLines Premium plugin.';
-		$object->url = $api_data['overview'];
 		$object->package = $this->build_url( $api_data, $data );
-		
+		$object->download_link = $this->build_url( $api_data, $data );
 		return $object;
+	}
+	
+	function check_info( $false, $action, $arg ){
+			
+		global $storeapi;
+		if( ! is_object( $storeapi ) )
+			$storeapi = new EditorStoreFront;
+
+		$mixed_array = $storeapi->get_latest();
+		
+		if( isset( $mixed_array[$arg->slug] ) ) {
+			$data = $mixed_array[$arg->slug];
+
+			$obj = new stdClass();
+			      $obj->slug = $data['slug'];
+			      $obj->plugin_name = $data['name'];
+			      $obj->new_version = $data['version'];
+			      $obj->requires = '3.6';
+			      $obj->tested = '3.6';
+			      $obj->downloaded = 0; // needs API update....
+			      $obj->last_updated = $data['last_mod'];
+			      $obj->sections = array(
+			        'description' => $this->build_desc( $data ),
+
+			        'changelog' => 'Not added yet!'
+			      );
+
+			      $obj->homepage = $data['overview'];
+			      return $obj;
+		} 
+		return false;
+	}
+	
+	
+	function build_desc( $data ) {
+		
+		$desc = sprintf( "<h1><img src='%s' /></h1>%s", $data['thumb'], $data['description'] );
+		return $desc;
 	}
 	
 	function build_url( $api_data, $data ) {
