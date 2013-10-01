e7f82625b9802ed8f739b23cecd5c3be2dfeabbd
Andrew Powers <andrew@pagelines.com>
2013-10-01 14:06:51 -0700

    upgrade popthumbs

--- sections/popthumbs/section.php
+++ sections/popthumbs/section.php
@@ -43,65 +43,95 @@ class PLPopThumbs extends PageLinesSection {
 			'type'	=> 'multi',
 			'opts'	=> array(
 				array(
-					'key'			=> 'popthumb_count',
-					'type' 			=> 'count_select',
-					'count_start'	=> 1,
-					'count_number'	=> 12,
-					'default'		=> 4,
-					'label' 	=> __( 'Number of PopThumbs to Configure', 'pagelines' ),
-				),
-				array(
 					'key'			=> 'popthumb_cols',
 					'type' 			=> 'count_select',
 					'count_start'	=> 1,
 					'count_number'	=> 12,
-					'default'		=> '3',
+					'default'		=> '4',
 					'label' 	=> __( 'Number of Columns for Each Thumb (12 Col Grid)', 'pagelines' ),
 				),
 			)
 
 		);
-
-		$slides = ($this->opt('popthumb_count')) ? $this->opt('popthumb_count') : $this->default_limit;
-	
-		for($i = 1; $i <= $slides; $i++){
-
-			$opts = array(
-
-				array(
-					'key'		=> 'popthumb_title_'.$i,
-					'label'		=> __( 'PopThumb Title', 'pagelines' ),
-					'type'		=> 'text'
-				),
-				array(
-					'key'		=> 'popthumb_text_'.$i,
-					'label'	=> __( 'PopThumb Text', 'pagelines' ),
-					'type'	=> 'textarea'
-				),
-			);
-
-			$opts[] = array(
-				'key'			=> 'popthumb_image_'.$i,
-				'label'			=> __( 'PopThumb Image', 'pagelines' ),
-				'type'			=> 'image_upload',
-				'sizelimit'		=> 800000,
-			);
+		
+		
+		$options[] = array(
+			'key'		=> 'popthumb_array',
+	    	'type'		=> 'accordion', 
+			'col'		=> 2,
+			'title'		=> __('PopThumbs Setup', 'pagelines'), 
+			'post_type'	=> __('Thumb', 'pagelines'), 
+			'opts'	=> array(
 			
-			$opts[] = array(
-				'key'			=> 'popthumb_thumb_'.$i,
-				'label'			=> __( 'PopThumb Thumb', 'pagelines' ),
-				'type'			=> 'image_upload',
-			);
+
+				array(
+					'key'	=> 'title',
+					'label'	=> __( 'Title', 'pagelines' ),
+					'type'			=> 'text'
+				),
+				array(
+					'key'	=> 'text',
+					'label'	=> __( 'Text', 'pagelines' ),
+					'type'			=> 'text'
+				),
+				
+				array(
+					'key'	=> 'thumb',
+					'label'	=> __( 'Thumb', 'pagelines' ),
+					'type'	=> 'image_upload'
+				),
+				array(
+					'key'	=> 'image',
+					'label'	=> __( 'Image', 'pagelines' ),
+					'type'	=> 'image_upload'
+				),
+				
+			
 
 
-			$options[] = array(
-				'title' 	=> __( 'PopThumb ', 'pagelines' ) . $i,
-				'type' 		=> 'multi',
-				'opts' 		=> $opts,
+			)
+	    );
 
-			);
-
-		}
+		// $slides = ($this->opt('popthumb_count')) ? $this->opt('popthumb_count') : $this->default_limit;
+		// 
+		// 	for($i = 1; $i <= $slides; $i++){
+		// 
+		// 		$opts = array(
+		// 
+		// 			array(
+		// 				'key'		=> 'popthumb_title_'.$i,
+		// 				'label'		=> __( 'PopThumb Title', 'pagelines' ),
+		// 				'type'		=> 'text'
+		// 			),
+		// 			array(
+		// 				'key'		=> 'popthumb_text_'.$i,
+		// 				'label'	=> __( 'PopThumb Text', 'pagelines' ),
+		// 				'type'	=> 'textarea'
+		// 			),
+		// 		);
+		// 
+		// 		$opts[] = array(
+		// 			'key'			=> 'popthumb_image_'.$i,
+		// 			'label'			=> __( 'PopThumb Image', 'pagelines' ),
+		// 			'type'			=> 'image_upload',
+		// 			'sizelimit'		=> 800000,
+		// 		);
+		// 		
+		// 		$opts[] = array(
+		// 			'key'			=> 'popthumb_thumb_'.$i,
+		// 			'label'			=> __( 'PopThumb Thumb', 'pagelines' ),
+		// 			'type'			=> 'image_upload',
+		// 		);
+		// 
+		// 
+		// 		$options[] = array(
+		// 			'title' 	=> __( 'PopThumb ', 'pagelines' ) . $i,
+		// 			'type' 		=> 'multi',
+		// 			'opts' 		=> $opts,
+		// 
+		// 		);
+		// 
+		// 	}
 
 		return $options;
 	}
@@ -110,49 +140,64 @@ class PLPopThumbs extends PageLinesSection {
    function section_template( ) { 
 	
 		$cols = ($this->opt('popthumb_cols')) ? $this->opt('popthumb_cols') : 3;
-		$num = ($this->opt('popthumb_count')) ? $this->opt('popthumb_count') : $this->default_limit;
+		
+		$item_array = $this->opt('popthumb_array');
+
+		$format_upgrade_mapping = array(
+			'title'			=> 'popthumb_title_%s',
+			'text'			=> 'popthumb_text_%s',
+			'image'			=> 'popthumb_image_%s',
+			'thumb'			=> 'popthumb_thumb_%s',
+		); 
+
+		$slide_array = $this->upgrade_to_array_format( 'popthumb_array', $item_array, $format_upgrade_mapping, $this->opt('popthumb_count'));
+		
+		$count = 1;
 		$width = 0;
 		$output = '';
-	
-		for($i = 1; $i <= $num; $i++):
-
-			$link = '';
-			$title = ($this->opt('popthumb_title_'.$i)) ? $this->opt('popthumb_title_'.$i) : 'PopThumb '.$i; 
-			$text = ($this->opt('popthumb_text_'.$i)) ? $this->opt('popthumb_text_'.$i) : ''; 
-			$img = '';
-			$thumb = ($this->opt('popthumb_title_'.$i));
-			
-			$attach_id = $this->opt('popthumb_image_'.$i.'_attach_id');
 		
+		$item_array = ( ! is_array($item_array) ) ? array( array(), array(), array() ) : $item_array; 
 			
-			if($this->opt('popthumb_image_'.$i)) {
-				
-				$full_img = $this->opt('popthumb_image_'.$i); 
-				
+		$num = count( $item_array );
+		
+		foreach( $item_array as $item ){
+	
+			$link = '';
+			
+			$title = pl_array_get( 'title', $item, 'PopThumb '.$count ); 
+			$text = pl_array_get( 'text', $item ); 
+			$img = '';
+	
+			$attach_id = pl_array_get( 'image_attach_id', $item ); 
+			
+			if( pl_array_get( 'image', $item ) ) {
+
+				$full_img = pl_array_get( 'image', $item ); 
+
 			} else 
 				$full_img = pl_default_image();
-				
-				
-				
-			if($this->opt('popthumb_thumb_'.$i)){
-				
-				$thumb_url = $this->opt('popthumb_thumb_'.$i);
-				
+
+
+
+			if( pl_array_get( 'thumb', $item ) ){
+
+				$thumb_url = pl_array_get( 'thumb', $item );
+
 			} elseif($attach_id && $attach_id != ''){
-				
+
 				$img = wp_get_attachment_image_src( $attach_id, 'basic-thumb'); 
-			
+
 				$thumb_url = $img[0]; 
-				
-			} elseif($this->opt('popthumb_image_'.$i)){
-				
-				$thumb_url = $this->opt('popthumb_image_'.$i);
-				
+
+			} elseif( pl_array_get( 'image', $item ) ){
+
+				$thumb_url = pl_array_get( 'image', $item );
+
 			} else
 				$thumb_url = pl_default_thumb();
-			
+
 			$thumb = sprintf('<img src="%s" />', $thumb_url);
-			
+
 
 			if($width == 0)
 				$output .= '<div class="row fix">';
@@ -162,7 +207,7 @@ class PLPopThumbs extends PageLinesSection {
 					<a class="popthumb" href="%s" rel="prettyPhoto[%s]">
 						<span class="popthumb-thumb pl-animation pl-appear pl-contrast">
 							%s
-							
+
 						</span>
 						<span class="expander"><i class="icon-plus"></i></span>
 					</a>
@@ -177,21 +222,103 @@ class PLPopThumbs extends PageLinesSection {
 				$full_img,
 				$this->meta['unique'], 
 				$thumb,
-				$i,
+				$count,
 				$title,
-				$i,
+				$count,
 				$text
 			);
 
 			$width += $cols;
 
-			if($width >= 12 || $i == $num){
+			if($width >= 12 || $count == $num){
 				$width = 0;
 				$output .= '</div>';
 			}
-
-
-		 endfor;
+			
+			$count++;
+	
+		}
+	
+	
+	
+		// for($i = 1; $i <= $num; $i++):
+		// 
+		// 		$link = '';
+		// 		$title = ($this->opt('popthumb_title_'.$i)) ? $this->opt('popthumb_title_'.$i) : 'PopThumb '.$i; 
+		// 		$text = ($this->opt('popthumb_text_'.$i)) ? $this->opt('popthumb_text_'.$i) : ''; 
+		// 		$img = '';
+		// 		$thumb = ($this->opt('popthumb_title_'.$i));
+		// 		
+		// 		$attach_id = $this->opt('popthumb_image_'.$i.'_attach_id');
+		// 	
+		// 		
+		// 		if($this->opt('popthumb_image_'.$i)) {
+		// 			
+		// 			$full_img = $this->opt('popthumb_image_'.$i); 
+		// 			
+		// 		} else 
+		// 			$full_img = pl_default_image();
+		// 			
+		// 			
+		// 			
+		// 		if($this->opt('popthumb_thumb_'.$i)){
+		// 			
+		// 			$thumb_url = $this->opt('popthumb_thumb_'.$i);
+		// 			
+		// 		} elseif($attach_id && $attach_id != ''){
+		// 			
+		// 			$img = wp_get_attachment_image_src( $attach_id, 'basic-thumb'); 
+		// 		
+		// 			$thumb_url = $img[0]; 
+		// 			
+		// 		} elseif($this->opt('popthumb_image_'.$i)){
+		// 			
+		// 			$thumb_url = $this->opt('popthumb_image_'.$i);
+		// 			
+		// 		} else
+		// 			$thumb_url = pl_default_thumb();
+		// 		
+		// 		$thumb = sprintf('<img src="%s" />', $thumb_url);
+		// 		
+		// 
+		// 		if($width == 0)
+		// 			$output .= '<div class="row fix">';
+		// 
+		// 		$output .= sprintf(
+		// 			'<div class="span%s fix">
+		// 				<a class="popthumb" href="%s" rel="prettyPhoto[%s]">
+		// 					<span class="popthumb-thumb pl-animation pl-appear pl-contrast">
+		// 						%s
+		// 						
+		// 					</span>
+		// 					<span class="expander"><i class="icon-plus"></i></span>
+		// 				</a>
+		// 				<div class="popthumb-text">
+		// 					<h4 data-sync="popthumb_title_%s">%s</h4>
+		// 					<div class="popthumb-desc" data-sync="popthumb_text_%s">
+		// 						%s
+		// 					</div>
+		// 				</div>
+		// 			</div>',
+		// 			$cols,
+		// 			$full_img,
+		// 			$this->meta['unique'], 
+		// 			$thumb,
+		// 			$i,
+		// 			$title,
+		// 			$i,
+		// 			$text
+		// 		);
+		// 
+		// 		$width += $cols;
+		// 
+		// 		if($width >= 12 || $i == $num){
+		// 			$width = 0;
+		// 			$output .= '</div>';
+		// 		}
+		// 
+		// 
+		// 	 endfor;
 	
 	
 	?>
