9dbef38a8b536bc410697f353dcd3c228ae7a29a
Andrew Powers <andrew@pagelines.com>
2013-10-18 15:54:27 -0700

    change from 'global options' to simply 'settings'

--- editor/panel.settings.php
+++ editor/panel.settings.php
@@ -24,7 +24,7 @@ class PageLinesSettingsPanel{
 	function toolbar( $toolbar ){
 
 		$toolbar[ 'settings' ] = array(
-			'name'	=> __( 'Global Options', 'pagelines' ),
+			'name'	=> __( 'Settings', 'pagelines' ),
 			'icon'	=> 'icon-globe',
 			'pos'	=> 35,
 			'panel'	=> $this->get_settings_tabs()
