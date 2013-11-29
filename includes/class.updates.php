b00ade092759ac00e64f1bb9eb20a347c109263a
Simon Prosser <simon@pagelines.com>
2013-11-29 20:17:52 +0000

    better updates js...

--- includes/class.updates.php
+++ includes/class.updates.php
@@ -30,7 +30,7 @@ class PageLinesUpdateCheck {
 
 		if( 'dms' != $folder )
 			return;
-		
+
 		add_action('admin_notices', array( $this,'pagelines_theme_update_nag') );
 		add_filter('site_transient_update_themes', array( $this,'pagelines_theme_update_push') );
 		add_filter('transient_update_themes', array( $this,'pagelines_theme_update_push') );
@@ -84,11 +84,11 @@ class PageLinesUpdateCheck {
 
 		$account_set_url = add_query_arg( array( 'tablink' => 'account', 'tabsublink' => 'pl_account#pl_account' ), site_url() );
 
-		$details_button = ( $pagelines_update['extra'] ) ? '<span style="float:right"><a class="pl_updates" href="#">Details</a></span>' : '';
+		$details_button = ( $pagelines_update['extra'] ) ? '<span style="float:right" class="pl_updates"><a href="#">Details</a></span><span style="float:right" class="pl_updates hidden"><a href="#">Hide</a></span>' : '';
 
 		$warning = ( $screen->id == 'update-core' ) ? '<br /><strong>Please</strong> update all plugins before upgrading DMS.' : '';
 
-		$details = ( $pagelines_update['extra'] ) ? sprintf( '<span id="pl_updates_data" style="display:none"><br />%s</span>', $pagelines_update['extra'] ) : '';
+		$details = ( $pagelines_update['extra'] ) ? sprintf( '<div id="pl_updates_data" style="display:none"><br />%s</div>', $pagelines_update['extra'] ) : '';
 
 		$content = sprintf( 'There is an update for DMS, version %s is now available. %s%s%s%s',
 
@@ -100,7 +100,13 @@ class PageLinesUpdateCheck {
 			$warning,
 			$details
 		 );
-		printf( '<div class="updated"><p>%s</p></div><script>jQuery( ".pl_updates" ).click(function() { jQuery( "#pl_updates_data" ).show( "slow" ) });</script>', $content );
+		printf( '<div class="updated"><p>%s</p></div>
+			<script>
+				jQuery( ".pl_updates" ).click(function() {
+					jQuery( "#pl_updates_data" ).toggle( "slow" )
+					jQuery( ".pl_updates" ).toggle()
+				})
+			</script>', $content );
 	}
 
 	/**
