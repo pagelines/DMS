f98e91b5e3a0b9ccf58799dd5a23c5296134fd5e
Andrew Powers <andrew@pagelines.com>
2013-12-16 16:40:27 -0800

    New comments function for theming.

--- includes/lib.elements.php
+++ includes/lib.elements.php
@@ -116,7 +116,48 @@ function pagelines_standard_sidebar($name, $description){
 }
 
 
+function pl_get_comments_template(){
+	if(!have_comments() && !comments_open()){
 
+		printf('<p class="nocomments">%s</p>', __('Comments are closed.', 'pagelines'));
+		return;
+
+	}
+
+	?>
+	<div id="comments" class="wp-comments">
+		<div class="wp-comments-pad">
+		<?php
+
+			/* Stop the rest of comments.php from being processed,
+			 * but don't kill the script entirely -- we still have
+			 * to fully load the template.
+			 */
+			if ( post_password_required() ){
+				printf('<p class="nopassword">%s</p></div></div>', __( 'This post is password protected. Enter the password to view any comments.', 'pagelines' ) );
+				return;
+			}
+
+			if ( have_comments() ) : ?>
+				<h5 id="comments-title"><?php
+				printf( _n( 'One Response to %2$s', '%1$s Responses to %2$s', get_comments_number(), 'pagelines' ),
+				number_format_i18n( get_comments_number() ), '"' . get_the_title() . '"' );
+				?></h5>
+			<ol class="commentlist">
+				<?php wp_list_comments( apply_filters( 'pl_list_comments', array( 'type'=> 'comment', 'avatar_size' => '60' ) ) ); ?>
+			</ol>
+			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+				<div class="navigation fix">
+					<div class="alignleft"><?php previous_comments_link( __( "<span class='meta-nav'>&larr;</span> Older Comments", 'pagelines' ) ); ?></div>
+					<div class="alignright"><?php next_comments_link( __( "Newer Comments <span class='meta-nav'>&rarr;</span>", 'pagelines' ) ); ?></div>
+				</div> <!-- .navigation -->
+			<?php endif; // check for comment navigation 
+
+			endif; // end have_comments()
+		comment_form(); ?>
+		</div>
+	</div><?php 
+}
 
 /**
  * PageLines Search Form
