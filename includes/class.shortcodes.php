8f4049f1483104fde83c5e3fb2f3c911327b6ede
Andrew Powers <andrew@pagelines.com>
2014-01-08 23:05:05 -0800

    switch up comments short code

--- includes/class.shortcodes.php
+++ includes/class.shortcodes.php
@@ -427,9 +427,9 @@ class PageLines_ShortCodes {
 	function pagelines_post_comments_shortcode( $atts ) {
 
 		$defaults = array(
-			'zero' => __( 'Add Comment', 'pagelines' ),
-			'one' => __( "<span class='num'>1</span> Comment", 'pagelines' ),
-			'more' => __( "<span class='num'>%</span> Comments", 'pagelines' ),
+			'zero' => __( "0 <i class='icon-comments'></i>", 'pagelines' ),
+			'one' => __( "1 <i class='icon-comments'></i>", 'pagelines' ),
+			'more' => __( "%s <i class='icon-comments'></i>", 'pagelines' ),
 			'hide_if_off' => 'disabled',
 			'before' 	=> '',
 			'after' 	=> '',
