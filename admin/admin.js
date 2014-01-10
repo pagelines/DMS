ed39a1a5966a105d3680eb8be34a995e89d8f3b5
Andrew Powers <andrew@pagelines.com>
2014-01-10 11:19:56 -0800

    uploader admin handling

--- admin/admin.js
+++ admin/admin.js
@@ -4,6 +4,8 @@
 	$(document).ready(function() {
 
 		$.plAdminMeta.init()
+		
+		$.plAdminMedia.init()
 
 	})
 
@@ -38,7 +40,149 @@
 			}
 			
 		}
+		
+		
 	}
+	
+	$.plAdminMedia = {
+		
+		init: function(){
+			
+			jQuery(".redux-opts-upload").click( function( event ) {
+		            var activeFileUploadContext = jQuery(this).parent();
+		            var relid = jQuery(this).attr('rel-id');
+					var $that = jQuery(this);
+					var elementID = jQuery(this).attr('id');
+
+		            event.preventDefault();
+
+		            // If the media frame already exists, reopen it.
+		            /*if ( typeof(custom_file_frame)!=="undefined" ) {
+		                custom_file_frame.open();
+		                return;
+		            }*/
+
+		            // if its not null, its broking custom_file_frame's onselect "activeFileUploadContext"
+		            custom_file_frame = null;
+
+		            // Create the media frame.
+		            custom_file_frame = wp.media.frames.customHeader = wp.media({
+		                // Set the title of the modal.
+		                title: jQuery(this).data("choose"),
+
+		                // Tell the modal to show only images. Ignore if want ALL
+		                library: {
+		                    type: 'image'
+		                },
+		                // Customize the submit button.
+		                button: {
+		                    // Set the text of the button.
+		                    text: jQuery(this).data("update")
+		                }
+		            });
+
+		            custom_file_frame.on( "select", function() {
+		                // Grab the selected attachment.
+		                var attachment = custom_file_frame.state().get("selection").first();
+
+		                // Update value of the targetfield input with the attachment url.
+		                jQuery('.redux-opts-screenshot',activeFileUploadContext).attr('src', attachment.attributes.url);
+		                jQuery('#' + relid ).val(attachment.attributes.url).trigger('change');
+
+		                jQuery('.redux-opts-upload',activeFileUploadContext).hide();
+		                jQuery('.redux-opts-screenshot',activeFileUploadContext).show();
+		                jQuery('.redux-opts-upload-remove',activeFileUploadContext).show();
+
+		                
+
+		        });
+
+		        custom_file_frame.open();
+		    });
+		
+		
+			jQuery(".redux-opts-upload-remove").click( function( event ) {
+			    var activeFileUploadContext = jQuery(this).parent();
+			    var relid = jQuery(this).attr('rel-id');
+
+			    event.preventDefault();
+
+			    jQuery('#' + relid).val('');
+			    jQuery(this).prev().fadeIn('slow');
+			    jQuery('.redux-opts-screenshot',activeFileUploadContext).fadeOut('slow');
+			    jQuery(this).fadeOut('slow');
+
+			     
+			});
+			
+			//media upload
+			jQuery(".redux-opts-media-upload").click( function( event ) {
+			            var activeFileUploadContext = jQuery(this).parent();
+			            var relid = jQuery(this).attr('rel-id');
+
+			            event.preventDefault();
+
+			            // If the media frame already exists, reopen it.
+			            /*if ( typeof(custom_file_frame)!=="undefined" ) {
+			                custom_file_frame.open();
+			                return;
+			            }*/
+
+			            // if its not null, its broking custom_file_frame's onselect "activeFileUploadContext"
+			            custom_file_frame = null;
+
+			            // Create the media frame.
+			            custom_file_frame = wp.media.frames.customHeader = wp.media({
+			                // Set the title of the modal.
+			                title: jQuery(this).data("choose"),
+
+			                // Tell the modal to show only images. Ignore if want ALL
+			                library: {
+			                    type: 'video'
+			                },
+			                // Customize the submit button.
+			                button: {
+			                    // Set the text of the button.
+			                    text: jQuery(this).data("update")
+			                }
+			            });
+
+			            custom_file_frame.on( "select", function() {
+			                // Grab the selected attachment.
+			                var attachment = custom_file_frame.state().get("selection").first();
+
+			                // Update value of the targetfield input with the attachment url.
+			                jQuery('#' + relid ).val(attachment.attributes.url).trigger('change');
+
+
+						    jQuery('#_nectar_video_embed').trigger('keyup');
+
+			                jQuery('.redux-opts-media-upload',activeFileUploadContext).hide();
+			                jQuery('.redux-opts-upload-media-remove',activeFileUploadContext).show();
+			        });
+
+			        custom_file_frame.open();
+			    });
+			
+			
+				jQuery(".redux-opts-upload-media-remove").click( function( event ) {
+				    var activeFileUploadContext = jQuery(this).parent();
+				    var relid = jQuery(this).attr('rel-id');
+
+				    event.preventDefault();
+
+				    jQuery('#' + relid).val('');
+				    jQuery(this).prev().fadeIn('slow');
+				    jQuery('.redux-opts-screenshot',activeFileUploadContext).fadeOut('slow');
+				    jQuery(this).fadeOut('slow');
+				});
+			
+			
+		}
+		
+	}
+	
+	 
 
 
 
