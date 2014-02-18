4652dcffb0ed9c52c5f499d14c70095e5a76267f
Evan Mattson <me@aaemnnost.tv>
2014-02-18 08:41:10 -0500

    replace jQuery with $ for consistency and readability

--- js/admin.pagelines.js
+++ js/admin.pagelines.js
@@ -11,26 +11,26 @@ $(document).ready(function(){
 
 	if($("#pl-dms-less").length){
 	
-		var cm_mode = jQuery("#pl-dms-less").data('mode')
-		,	cm_config = jQuery.extend( {}, cm_base_config, { mode : cm_mode } )
-		,	editor3 = CodeMirror.fromTextArea(jQuery("#pl-dms-less").get(0), cm_config)
+		var cm_mode = $("#pl-dms-less").data('mode')
+		,	cm_config = $.extend( {}, cm_base_config, { mode : cm_mode } )
+		,	editor3 = CodeMirror.fromTextArea($("#pl-dms-less").get(0), cm_config)
 		
 	}
 
-	if(jQuery("#pl-dms-scripts").length){
+	if($("#pl-dms-scripts").length){
 		
-		var cm_mode = jQuery("#pl-dms-scripts").data('mode')
-		,	cm_config = jQuery.extend( {}, cm_base_config, { mode : cm_mode } )
-		,	editor4 = CodeMirror.fromTextArea(jQuery("#pl-dms-scripts").get(0), cm_config);
+		var cm_mode = $("#pl-dms-scripts").data('mode')
+		,	cm_config = $.extend( {}, cm_base_config, { mode : cm_mode } )
+		,	editor4 = CodeMirror.fromTextArea($("#pl-dms-scripts").get(0), cm_config);
 	}
 	
-	jQuery('.dms-update-setting').on('submit', function(e){
+	$('.dms-update-setting').on('submit', function(e){
 		
-		var theSetting = jQuery(this).data('setting')
-		,	theValue = jQuery('.input_'+theSetting).val()
-		,	saveText = jQuery(this).find('.saving-confirm');
+		var theSetting = $(this).data('setting')
+		,	theValue = $('.input_'+theSetting).val()
+		,	saveText = $(this).find('.saving-confirm');
 
-		jQuery.ajax({
+		$.ajax({
 			type: 'POST',
 			url: ajaxurl,
 			data: {
@@ -162,14 +162,14 @@ $(document).ready(function(){
  * ###########################
  */
 
-jQuery.fn.center = function ( relative_element ) {
+$.fn.center = function ( relative_element ) {
 
     this.css("position","absolute");
-    this.css("top", ( jQuery(window).height() - this.height() ) / 4+jQuery(window).scrollTop() + "px");
-    this.css("left", ( jQuery(relative_element).width() - this.width() ) / 2+jQuery(relative_element).scrollLeft() + "px");
+    this.css("top", ( $(window).height() - this.height() ) / 4+$(window).scrollTop() + "px");
+    this.css("left", ( $(relative_element).width() - this.width() ) / 2+$(relative_element).scrollLeft() + "px");
     return this;
 }
 
-jQuery.fn.exists = function(){return jQuery(this).length>0;}
+$.fn.exists = function(){return $(this).length>0;}
 
 }(window.jQuery);
