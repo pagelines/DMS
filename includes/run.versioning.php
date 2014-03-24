4aa8c0f40152cb061e884fe3ad970710a690b9a2
Simon Prosser <simon@pagelines.com>
2014-03-24 20:56:22 +0000

    redirect on upgrade to 2.0

--- includes/run.versioning.php
+++ includes/run.versioning.php
@@ -10,13 +10,24 @@ $actual = PL_CORE_VERSION;
 
 // if new version do some housekeeping.
 if ( version_compare( $actual, $installed ) > 0 ) {
-		delete_transient( 'pagelines_theme_update' );
-		delete_transient( 'pagelines_extend_themes' );
-		delete_transient( 'pagelines_extend_sections' );
-		delete_transient( 'pagelines_extend_plugins' );
-		delete_transient( 'pagelines_extend_integrations' );
-		delete_transient( 'pagelines_sections_cache' );
-		define( 'PL_CSS_FLUSH', true );
+	$dms_cache = new DMS_Cache;
+
+	// we want to purge the cache during a theme update.
+	$dms_cache->purge_all();
+	
+	// special function that runs once during upgrade from 1.x to 2.x series.
+	if( version_compare( $installed, '1.5.0', '<' ) ) {
+		add_action( 'after_setup_theme', 'dms_update_two_point_ohh' );
+	}
+}
+
+function dms_update_two_point_ohh() {
+		
+	// run the 2.0 install routine.
+	$install = new PageLinesInstall;
+	$url = $install->run_installation_routine();
+	wp_safe_redirect( $url ); 
+	exit;
 }
 set_theme_mod( 'pagelines_version', $actual );
 set_theme_mod( 'pagelines_child_version', pl_get_theme_data( get_stylesheet_directory(), 'Version' ) );
