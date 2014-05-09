75320610d1fb9e086e301a849f3bdac33514cf0a
Simon Prosser <simon@pagelines.com>
2014-05-09 01:28:31 +0100

    fix

--- phpunit/bootstrap.php
+++ phpunit/bootstrap.php
@@ -3,4 +3,5 @@
 // This has to fire up wordpress..
 // If you are using symlinks etc locally you WILL have to edit this and add the real path.
 include( '../../../../wp-load.php' );
+switch_theme('dms');
 
