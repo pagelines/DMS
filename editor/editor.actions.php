01a4ae865f72cc3f8b5a7989ecda99864b554aa0
Simon Prosser <simon@pagelines.com>
2014-07-22 00:37:38 +0100

    remove bogus plPrint and revert the live/draft thing.
    Conflicts:
    	js/admin.pagelines.js

--- editor/editor.actions.php
+++ editor/editor.actions.php
@@ -221,7 +221,7 @@ function pl_dms_admin_actions(){
 		}
 	}
 
-	pl_setting_update($field, $value, 'global', 'live' );
+	pl_setting_update($field, $value );
 
 	$response['value'] = $value;
 	$response['field'] = $field;
