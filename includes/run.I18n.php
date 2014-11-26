98c1224b785c0071d315ba5f638a47693c6e2826
Simon <simon@pagelines.com>
2014-11-26 18:11:46 +0000

    run translation code during after_setup_theme action

--- includes/run.I18n.php
+++ includes/run.I18n.php
@@ -2,7 +2,14 @@
 /*
  *	Setup DMS Localization
  */
-load_theme_textdomain( 'pagelines', PAGELINES_CORE_LANG_DIR );
 
-if( defined( 'PAGELINES_THEME_LANG_DIR' ) )
-	load_theme_textdomain( 'pagelines', PAGELINES_THEME_LANG_DIR );
+add_action( 'after_setup_theme', 'dms_run_i18n' );
+
+function dms_run_i18n() {
+	load_theme_textdomain( 'pagelines', PAGELINES_CORE_LANG_DIR );
+
+	if( defined( 'PAGELINES_THEME_LANG_DIR' ) ) {
+		$a = load_theme_textdomain( 'pagelines', 'language' );
+		var_dump( $a );
+	}
+}
