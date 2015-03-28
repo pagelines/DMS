56d541a0f79adf5e55abad13d7729b8210da620c
Simon <pross@pross.org.uk>
2015-03-28 23:01:24 +0000

    use relative admin urls for ajax, fixes SSL/karma

--- includes/lib.head.php
+++ includes/lib.head.php
@@ -40,19 +40,19 @@ function pl_js_wrap( $js ){
  **/
 add_action('pagelines_after_page', 'pl_region_footer');
 function pl_region_footer(){
-	
+
 	// allow users to disable
 	if( pl_setting('region_disable_footer') )
-		return; 
-		
+		return;
+
 	pagelines_register_hook('pagelines_before_footer'); // Hook ?>
 	<footer id="footer" class="footer pl-region" data-region="footer">
 		<div class="page-area outline pl-area-container fix">
 		<?php pagelines_template_area('pagelines_footer', 'footer'); // Hook ?>
 		</div>
 	</footer>
-	
-	<?php 
+
+	<?php
 }
 
 
@@ -61,19 +61,19 @@ function pl_region_footer(){
  **/
 add_action('pagelines_before_main', 'pl_region_header');
 function pl_region_header(){
-	
+
 	// allow users to disable
 	if( pl_setting('region_disable_header') )
-		return; 
-		
+		return;
+
 	pagelines_register_hook('pagelines_before_header');	?>
 	<header id="header" class="header pl-region" data-region="header">
 		<div class="outline pl-area-container">
 			<?php pagelines_template_area('pagelines_header', 'header'); // Hook ?>
 		</div>
 	</header>
-	
-	<?php 
+
+	<?php
 }
 
 
@@ -82,27 +82,27 @@ function pl_region_header(){
  **/
 add_action('pagelines_site_wrap', 'pl_region_fixed');
 function pl_region_fixed(){
-	
+
 	// allow users to disable
 	if( pl_setting('region_disable_fixed') )
-		return; 
-		
+		return;
+
 	?>
 	<div id="fixed-top" class="pl-fixed-top is-not-fixed" data-region="fixed-top">
-		
+
 		<div class="pl-fixed-region pl-region" data-region="fixed">
 			<div class="outline pl-area-container">
 				<?php pagelines_template_area('pagelines_fixed_top', 'fixed_top'); // Hook ?>
 				<?php pagelines_template_area('pagelines_fixed', 'fixed'); // Hook ?>
-				
+
 			</div>
 		</div>
-		
+
 	</div>
 	<div class="fixed-top-pusher"></div>
 	<script> jQuery('.fixed-top-pusher').height( jQuery('.pl-fixed-top').height() ) </script>
-	
-	<?php 
+
+	<?php
 }
 
 add_filter( 'user_contactmethods', 'pagelines_add_google_profile', 10, 1);
@@ -129,13 +129,13 @@ function pagelines_google_author_head() {
 add_action( 'wp_enqueue_scripts', 'pagelines_register_js' );
 function pagelines_register_js() {
 
-	
+
 	wp_enqueue_script( 'pagelines-bootstrap-all', PL_JS . '/script.bootstrap.min.js', array( 'jquery' ), '2.2.2', true );
-	
+
 	wp_enqueue_script( 'pagelines-helpers', PL_JS . '/pl.helpers.js', array( 'jquery' ), pl_get_cache_key(), true );
 
 	wp_enqueue_script( 'pagelines-fitvids', PL_JS . '/script.fitvids.js', array( 'jquery' ), pl_get_cache_key(), true );
-	
+
 	wp_enqueue_script( 'pagelines-common', PL_JS . '/pl.common.js', array( 'jquery' ), pl_get_cache_key(), true );
 
 	// Load Supersize BG Script
@@ -143,9 +143,9 @@ function pagelines_register_js() {
 
 	wp_register_script( 'pl-chosen', PL_JS . '/chosen/chosen.jquery.min.js', array( 'jquery' ), pl_get_cache_key(), false );
 	wp_register_style( 'pl-chosen', PL_JS . '/chosen/chosen.css', pl_get_cache_key() );
-	
 
-	wp_localize_script( 'pagelines-common', 'plajaxrequest', array( pl_admin_url( 'admin-ajax.php' ) ) );
+
+	wp_localize_script( 'pagelines-common', 'plajaxrequest', array( admin_url( 'admin-ajax.php', 'relative' ) ) );
 }
 
 /**
@@ -182,15 +182,15 @@ function pagelines_head_common(){
 		// RTL Language Support
 
 		// wordpress autoloads from child theme so if child theme has no rtl we need to load ours.
-		if( 
-			( is_rtl() 
-				&& is_child_theme() 
-				&& ! is_file( sprintf( '%s/rtl.css', get_stylesheet_directory() ) ) 
-			) || ( is_rtl() && ! is_child_theme() ) 
+		if(
+			( is_rtl()
+				&& is_child_theme()
+				&& ! is_file( sprintf( '%s/rtl.css', get_stylesheet_directory() ) )
+			) || ( is_rtl() && ! is_child_theme() )
 		){
 			add_action( 'wp_print_styles', create_function( '', 'pagelines_load_css_relative( "rtl.css", "pagelines-rtl" );' ), 99 );
 		}
-			
+
 	}
 
 	if ( pl_setting( 'facebook_headers' ) && ! has_action( 'disable_facebook_headers' ) && VPRO )
@@ -202,7 +202,7 @@ function pagelines_head_common(){
 		wp_enqueue_style( 'prettify', PL_JS . '/prettify/prettify.css' );
 		add_action( 'wp_head', create_function( '',  'echo pl_js_wrap("prettyPrint()");' ), 14 );
 	}
-	
+
 	add_action( 'wp_head', create_function( '',  'echo pl_source_comment("Start >> Meta Tags and Inline Scripts", 2);' ), 0 );
 
 	add_action( 'wp_print_styles', create_function( '',  'echo pl_source_comment("Styles");' ), 0 );
@@ -245,13 +245,13 @@ function pagelines_meta_tags(){
 	// Removes viewport scaling on Phones, Tablets, etc.
 	if(!pl_setting('disable_mobile_view', $oset) && !apply_filters( 'disable_mobile_view', '' ))
 		echo '<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />';
-		
-	
+
+
 	printf( "\n<meta property='pl-share-title' content='%s' />\n", get_the_title($pagelines_ID));
 	printf( "<meta property='pl-share-url' content='%s' />\n", get_permalink($pagelines_ID));
 	printf( "<meta property='pl-share-desc' content='%s' />\n", pl_short_excerpt( $pagelines_ID, 15 ));
 	printf( "<meta property='pl-share-img' content='%s' />\n", pl_the_thumbnail_url( $pagelines_ID ));
-	
+
 }
 
 
@@ -394,7 +394,3 @@ function pagelines_filter_wp_title( $title ) {
 	}
     return apply_filters( 'pagelines_meta_title', $new_title );
 }
-
-
-
-
--- includes/utils.karma.php
+++ includes/utils.karma.php
@@ -2,99 +2,99 @@
 
 
 class PLKarma {
-	
+
 	var $option_key = '_pl_karma';
-	
-	 function __construct()   {	
-		
+
+	 function __construct()   {
+
         add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ), 11 );
         add_action( 'wp_ajax_pl_karma', array( $this, 'ajax' ) );
 		add_action( 'wp_ajax_nopriv_pl_karma', array( $this, 'ajax' ) );
-		
+
 	}
-	
+
 	function enqueue_scripts() {
-		
-		wp_localize_script( 'pagelines-common', 'plKarma', array( 'ajaxurl' => admin_url('admin-ajax.php') ));
-		
+
+		wp_localize_script( 'pagelines-common', 'plKarma', array( 'ajaxurl' => admin_url( 'admin-ajax.php', 'relative' ) ) );
+
 	}
-	
+
 	function ajax($post_id) {
-		
+
 		$field_id = ( isset( $_POST['karma_id'] ) ) ? $_POST['karma_id'] : false;
-		
+
 		if( $field_id ) {
-			
+
 			$post_id = str_replace('pl-karma-', '', $_POST['karma_id']);
 			echo $this->karma_post($post_id, 'update');
-		
+
 		} else {
 			$post_id = str_replace('pl-karma-', '', $_POST['karma_id']);
 			echo $this->karma_post($post_id, 'get');
 		}
-		
+
 		exit;
 	}
-	
-	
+
+
 	function karma_post($post_id, $action = 'get') {
-	
-		if( ! is_numeric($post_id) ) 
+
+		if( ! is_numeric($post_id) )
 			return;
-	
+
 		$karma_count = get_post_meta($post_id, $this->option_key, true);
-	
+
 		switch($action) {
-		
+
 			case 'get':
-			
-				
+
+
 				if( ! $karma_count ){
-					
+
 					$karma_count = 0;
-					
+
 					add_post_meta($post_id, $this->option_key, $karma_count, true);
-				
+
 				}
-				
+
 				return $karma_count;
-				
+
 				break;
-				
+
 			case 'update':
-				
-				
+
+
 				if( isset($_COOKIE[ $this->option_key. $post_id]) ) return $karma_count;
-				
+
 				$karma_count++;
-				
+
 				update_post_meta( $post_id, $this->option_key, $karma_count );
-				
+
 				setcookie( $this->option_key. $post_id, $post_id, time()*20, '/');
-				
+
 				return $karma_count;
 				break;
-		
+
 		}
 	}
 
 
 	function add_karma( $id = false, $args = array() ) {
-		
+
 		$defaults = array(
 			'classes'	=> '',
 			'attr'		=> '',
 			'icon'		=> ''
 		);
-		
-		$atts = wp_parse_args( $args, $defaults ); 
-		
+
+		$atts = wp_parse_args( $args, $defaults );
+
 		global $post;
-		
+
 		$id = ($id) ? $id : $post->ID;
 
 		$output = $this->karma_post($id);
-  
+
   		$class = 'pl-karma pl-social-counter pl-social-pagelines';
 
   		$title = __('Give Karma', 'pagelines');
@@ -103,13 +103,13 @@ class PLKarma {
 			$class = 'pl-karma loved';
 			$title = __('You already gave karma!', 'pagelines');
 		}
-		
+
 		$karma_icon = ( '' != pl_setting( 'karma_icon' ) ) ? pl_setting( 'karma_icon' ) : 'sun';
 		if( '' != $atts['icon'] )
 			$karma_icon = $atts['icon'];
-		
-		return sprintf('<a href="#" class="%s %s" id="pl-karma-%s" title="%s" data-social="pagelines" %s> <span class="pl-social-icon"><i class="icon icon-%s"></i></span> <span class="pl-social-count">%s</span></a>', 
-						$class, 
+
+		return sprintf('<a href="#" class="%s %s" id="pl-karma-%s" title="%s" data-social="pagelines" %s> <span class="pl-social-icon"><i class="icon icon-%s"></i></span> <span class="pl-social-count">%s</span></a>',
+						$class,
 						$atts['classes'],
 						$id,
 						$title,
@@ -117,9 +117,9 @@ class PLKarma {
 						$karma_icon,
 						$output
 					);
-		
+
 	}
-	
+
 }
 
 
@@ -127,10 +127,9 @@ global $pl_karma;
 $pl_karma = new PLKarma();
 
 function pl_karma( $id = false, $args = array() ) {
-	
+
 	global $pl_karma;
 
-	return $pl_karma->add_karma( $id, $args ); 
-	
-}
+	return $pl_karma->add_karma( $id, $args );
 
+}
